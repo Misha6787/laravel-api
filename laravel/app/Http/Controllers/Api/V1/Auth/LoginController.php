ce6fc2d36b49d9d9a8dc5b8dda3dfe329c79291b
Миша Филимон <59843351+Misha6787@users.noreply.github.com>
2023-02-28 00:00:17 +0300

    add comments

--- laravel/app/Http/Controllers/Api/V1/Auth/LoginController.php
+++ laravel/app/Http/Controllers/Api/V1/Auth/LoginController.php
@@ -3,121 +3,38 @@
 namespace App\Http\Controllers\Api\V1\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
-use Carbon\Carbon;
-use Illuminate\Auth\AuthenticationException;
+use App\Http\Requests\V1\LoginFormRequest;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Testing\Fluent\Concerns\Has;
-use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
 {
     /**
+     * Авторизация пользователя
+     * @return JsonResponse
      */
-    public function login(Request $request)
+    public function login(LoginFormRequest $request)
     {
-//        return $request;
-        $credentials = $request->validate([
-            'email' => ['required', 'email'],
-            'password' => ['required'],
-        ]);
-
+        // Получаем отправленные данные - почту и пароль
+        $credentials = $request->all();
+        // Аутификация пользователя, ищем пользователя в базе данных, если не находим или неправильный пароль отправляем ошибку
+        // также если пользователь был найден то авторизируем его
         if (!Auth::attempt($credentials)) {
             return response()->json([
                 'message' => 'You cannot sign with those credentials',
                 'errors' => 'Unauthorised'
             ], 401);
         }
-
+        // Получаем авторизированного пользователя
         $auth = Auth::user();
-
+        // Создаем новый токен доступа для пользователя
         $token = $auth->createToken(config('app.name'));
-
-//            $token->token->save();
 
         return response()->json([
             'token_type' => 'Bearer',
             'token' => $token->plainTextToken,
             'user' => $auth,
         ], 200);
-
-            //$request->session()->regenerate();
-
-            // Auth::user() === $request->user()
-
-//            $token = $request->user()->createToken();
-
-
-//            $auth = Auth::user();
-//
-//            $token = $auth->createToken(config('app.name'));
-////            $token->token->expires_at = $request->remember_me ?
-////                Carbon::now()->addMonth() :
-////                Carbon::now()->addDay();
-//
-//            $token->token->save();
-//
-//            return response()->json([
-//                'token_type' => 'Bearer',
-//                'token' => $token->accessToken,
-//                'expires_at' => Carbon::parse($token->token->expires_at)->toDateTimeString(),
-//                'user' => $auth,
-//            ], 200);
-
-
-
-//        $this->middleware('guest')->except('logout');
-
-//        if (!Auth::attempt($request->only('email', 'password'))) {
-//            return response()->json([
-//                'message' => 'Invalid login details'
-//            ], 204);
-//        }
-//        $request->session()->regenerate();
-//        return [];
-
-
-//        $user = User::where('email', $request->email)->first();
-//
-//        return Hash::check($request->password, $user->password);
-
-
-
-
-
-
-
-//        if (Hash::check($pass, User::find(4)->password)) {
-//            return 1;
-//        } else {
-//            return 0;
-//        }
-
-//        $request->validate([
-//                'email' => ['required', 'string', 'max:255'],
-//                'password' => ['required', 'string'],
-//            ]);
-//
-//        $credentials = $request->only('email', 'password');
-//        if (Auth::attempt($credentials)) {
-//            // Authentication passed...
-//           return response()->json([
-//               'message' => 'Logged in Successfully.'
-//           ], 201);
-//        }
-//        else{
-//            throw ValidationException::withMessages([
-//                'email' => 'Invalid Credentials'
-//            ]);
-//        }
-    }
-    public function logout()
-    {
-       if(auth()->check()){
-        auth()->logout();
-        return "Logged Out";
-       }
     }
 }
--- laravel/app/Http/Controllers/Api/V1/Auth/RegisterController.php
+++ laravel/app/Http/Controllers/Api/V1/Auth/RegisterController.php
@@ -4,38 +4,33 @@ namespace App\Http\Controllers\Api\V1\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\V1\RegisterFormRequest;
-use App\Http\Resources\V1\RegisterResource;
 use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
+    /**
+     * Регистрация пользователя
+     * также тут пройсходит и его аутификация
+     * @return JsonResponse
+     */
     public function register(RegisterFormRequest $request)
     {
-//        $user = new RegisterResource();
-
-
         $user = User::create($request->all());
 
+        // Авторизируем нового пользователя
         Auth::login($user);
-
+        // Получаем нового пользователя
         $auth = Auth::user();
-
+        // Создаем новый токен доступа для пользователя
         $token = $auth->createToken(config('app.name'));
-
-//            $token->token->save();
 
         return response()->json([
             'token_type' => 'Bearer',
             'token' => $token->plainTextToken,
             'user' => $auth,
         ], 200);
-    }
-    public function users()
-    {
-        $users = User::all();
-        return response()->json($users);
     }
 }
